855b8ecf9feb379341041373da25be364da2de1b
Ernie Wilson <erniewilson@Ernies-MacBook-Air.local>
2026-04-24 23:28:37 -0400

    Add AdSense meta verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-7337526374337325"
+        />
+      </head>
       <body>
         <Script
           async
